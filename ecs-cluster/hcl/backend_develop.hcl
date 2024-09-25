46890f0b07fe622a29c93ec65ac4568f3c5e36df
ashkan-farivarmoheb <ashkan.farivarmoheb@gmail.com>
2024-09-25 23:23:48 +1000

    create ecs cluster

---
+++ ecs-cluster/hcl/backend_develop.hcl
@@ -0,0 +1,4 @@
+bucket         = "develop-ecs-cluster-odoo-bucket"
+region         = "ap-southeast-2"
+dynamodb_table = "develop-ecs-cluster-odoo-terraform-lock"
+encrypt        = true
---
+++ ecs-cluster/main.tf
@@ -0,0 +1,39 @@
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${var.environment}-${var.project}"
+  setting {
+    name = "containerInsights"
+    value = "disabled"
+  }
+  configuration {
+    execute_command_configuration {
+      logging = "DEFAULT"
+    }
+  }
+}
+
+resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+ name = "${var.environment}-${var.project}-capacity-provider"
+
+ auto_scaling_group_provider {
+   auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
+
+   managed_scaling {
+     status                    = "ENABLED"
+     target_capacity           = 100
+   }
+   managed_termination_protection = "DISABLED"
+ }
+}
+
+
+resource "aws_ecs_cluster_capacity_providers" "ecs_cluster_capacity_provider" {
+ cluster_name = aws_ecs_cluster.ecs_cluster.name
+ capacity_providers = [ aws_ecs_capacity_provider.ecs_capacity_provider.name ]
+
+ default_capacity_provider_strategy {
+   base              = 0
+   weight            = 1
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+ }
+  depends_on = [ aws_ecs_cluster.ecs_cluster ]
+}
