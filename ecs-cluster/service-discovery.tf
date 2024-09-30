325c4634213eaac8b9ba5bcde573375d4661ffed
ashkan-farivarmoheb <ashkan.farivarmoheb@gmail.com>
2024-09-30 23:51:49 +1000

    create ecs cluster

--- ecs-cluster/service-discovery.tf
+++ ecs-cluster/service-discovery.tf
@@ -1,24 +1,24 @@
-resource "aws_service_discovery_private_dns_namespace" "service_discovery_dns" {
-  name        = "${var.environment}-${var.project}"
-  description = "${var.environment}-${var.project} private dns namespace"
-  vpc         = data.aws_vpc.odoo.id
-}
+# resource "aws_service_discovery_private_dns_namespace" "service_discovery_dns" {
+#   name        = "${var.environment}-${var.project}"
+#   description = "${var.environment}-${var.project} private dns namespace"
+#   vpc         = data.aws_vpc.odoo.id
+# }
 
-resource "aws_service_discovery_service" "service_discovery" {
-  name = "${var.environment}-${var.project}-discovery"
+# resource "aws_service_discovery_service" "service_discovery" {
+#   name = "${var.environment}-${var.project}-discovery"
 
-  dns_config {
-    namespace_id = aws_service_discovery_private_dns_namespace.service_discovery_dns.id
+#   dns_config {
+#     namespace_id = aws_service_discovery_private_dns_namespace.service_discovery_dns.id
 
-    dns_records {
-      ttl  = 10
-      type = "A"
-    }
+#     dns_records {
+#       ttl  = 10
+#       type = "A"
+#     }
 
-    routing_policy = "MULTIVALUE"
-  }
+#     routing_policy = "MULTIVALUE"
+#   }
 
-  health_check_custom_config {
-    failure_threshold = 1
-  }
-}
+#   health_check_custom_config {
+#     failure_threshold = 1
+#   }
+# }
